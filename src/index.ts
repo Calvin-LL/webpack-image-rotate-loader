c4ed10b51e120badb0ee5388e06cb8e994104cba
Calvin Liang <me@calvin.sh>
2020-11-15 21:04:37 -0800

    feat: add options

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import path from "path";
-
 import fileLoader from "file-loader";
 import loaderUtils from "loader-utils";
-import mime from "mime";
-import replaceExt from "replace-ext";
 import { validate } from "schema-utils";
 import { Schema } from "schema-utils/declarations/validate";
 import sharp from "sharp";
@@ -13,42 +9,8 @@ import { loader } from "webpack";
 import schema from "./options.json";
 
 export interface OPTIONS {
-  width?: number;
-  height?: number;
-  fit?: "cover" | "contain" | "fill" | "inside" | "outside";
-  position?:
-    | "top"
-    | "right top"
-    | "right"
-    | "right bottom"
-    | "bottom"
-    | "left bottom"
-    | "left"
-    | "left top"
-    | "north"
-    | "northeast"
-    | "east"
-    | "southeast"
-    | "south"
-    | "southwest"
-    | "west"
-    | "northwest"
-    | "center"
-    | "centre"
-    | "entropy"
-    | "attention";
   background?: string | object;
-  scale?: number;
-  format?: "jpeg" | "png" | "webp" | "tiff";
-  quality?: number;
-  scaleUp?: boolean;
-  sharpOptions?: {
-    resize?: object;
-    png?: object;
-    jpeg?: object;
-    webp?: object;
-    tiff?: object;
-  };
+  angle?: number;
   fileLoaderOptions?: object;
 }
 
@@ -64,29 +26,24 @@ export default function (
   const queryObject = this.resourceQuery
     ? (loaderUtils.parseQuery(this.resourceQuery) as Partial<OPTIONS>)
     : undefined;
-  const fullOptions = { ...options, ...queryObject };
+  const fullOptions = {
+    ...options,
+    ...attemptToConvertValuesToNumbers(queryObject),
+  };
 
   validate(schema as Schema, fullOptions, {
-    name: "Image Resize Loader",
+    name: "Image Rotate Loader",
     baseDataPath: "options",
   });
 
-  const fullOptionsWithDefaults = {
-    format: getFormat(this.resourcePath),
-    scaleUp: false,
-    ...fullOptions,
-  };
-
-  processImage(content, fullOptionsWithDefaults)
+  processImage(content, fullOptions)
     .then((result) => {
       const fileLoaderContext = {
         ...this,
-        resourcePath: replaceExtension(
-          this.resourcePath,
-          fullOptionsWithDefaults.format
-        ),
-        query: fullOptionsWithDefaults.fileLoaderOptions,
+        resourcePath: this.resourcePath,
+        query: fullOptions.fileLoaderOptions,
       };
+
       const fileLoaderResult = fileLoader.call(
         fileLoaderContext,
         result,
@@ -101,78 +58,24 @@ export default function (
 
 async function processImage(
   content: ArrayBuffer,
-  {
-    width,
-    height,
-    fit,
-    position,
-    background,
-    scale,
-    format,
-    quality,
-    scaleUp,
-    sharpOptions,
-  }: Readonly<OPTIONS>
+  { background, angle }: Readonly<OPTIONS>
 ) {
   let sharpImage = sharp(Buffer.from(content));
-  const {
-    height: imageHeight,
-    width: imageWidth,
-  } = await sharpImage.metadata();
-  const normalizedImageHeight = imageHeight ?? Number.MAX_VALUE;
-  const normalizedImageWidth = imageWidth ?? Number.MAX_VALUE;
-  const normalizedResultHeight = height ?? 0;
-  const normalizedResultWidth = width ?? 0;
 
-  if (
-    (width || height) &&
-    (scaleUp ||
-      (normalizedResultHeight <= normalizedImageHeight &&
-        normalizedResultWidth <= normalizedImageWidth))
-  )
-    sharpImage = sharpImage.resize({
-      width,
-      height,
-      fit,
-      position,
-      background,
-      ...sharpOptions?.resize,
-    });
-  else if (scale && (scaleUp || scale <= 1)) {
-    sharpImage = sharpImage.resize({
-      width: Math.round(normalizedImageWidth * scale),
-      fit,
-      position,
-      background,
-      ...sharpOptions?.resize,
-    });
-  }
-
-  if (format)
-    sharpImage = sharpImage[format]({ quality, ...sharpOptions?.[format] });
+  if (background) sharpImage = sharpImage.rotate(angle, { background });
+  else sharpImage = sharpImage.rotate(angle);
 
   return await sharpImage.toBuffer();
 }
 
-function replaceExtension(resourcePath: string, format: string | undefined) {
-  if (!format) return resourcePath;
-  if (mime.getType(format) === mime.getType(path.extname(resourcePath)))
-    return resourcePath;
+function attemptToConvertValuesToNumbers(object: any | undefined) {
+  const result = { ...object };
 
-  return replaceExt(resourcePath, `.${format}`);
-}
+  Object.keys(result).forEach((key) => {
+    if (/^\d+$/.test(result[key])) {
+      result[key] = Number(result[key]);
+    }
+  });
 
-function getFormat(resourcePath: string): OPTIONS["format"] | undefined {
-  const type = mime.getType(resourcePath);
-
-  switch (type) {
-    case "image/jpeg":
-      return "jpeg";
-    case "image/png":
-      return "png";
-    case "image/webp":
-      return "webp";
-    case "image/tiff":
-      return "tiff";
-  }
+  return result;
 }
